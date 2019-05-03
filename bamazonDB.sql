08a9639154cf5c7bd3c14d506c3dd02e08919e37
christopher bell <christopherbell@christophers-MacBook-Air.local>
2019-05-03 15:40:28 -0400

    cleaned up db code and started to write node functionality

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -21,36 +21,20 @@ CREATE TABLE products (
   PRIMARY KEY (item_id)
 );
 
--- Creates new rows containing data in all named columns --
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("bluetooth wireless mouse", "Electronics", 18.49, 14);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("disc golf starter set", "Sporting Goods", 35.99, 3);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("febreeze: 128oz. bucket", "Household", 10.49, 28);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("febreeze: 5 gal. bucket", "Household", 45.49, 12);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("CDR rewritable discs: 10pk.", "Electronics", 6.49, 82);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Ladder Golf Backyard Set", "Sporting Goods", 38.99, 9);
-
 -- ### Alternative way to insert more than one row
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES 
+("bluetooth wireless mouse", "Electronics", 18.49, 14),
+("disc golf starter set", "Sporting Goods", 35.99, 3),
+("febreeze: 128oz. bucket", "Household", 10.49, 28),
+("febreeze: 5 gal. bucket", "Household", 45.49, 12),
+("CDR rewritable discs: 10pk.", "Electronics", 6.49, 82),
+("Ladder Golf Backyard Set", "Sporting Goods", 38.99, 9),
 ("Flip Cup/Beer Pong Party Set", "Parties & Entertainment", 17.00, 15),
 ("Thigh Master", "Sporting Goods", 82.98, 90),
 ("Massage Wand", "Electronics", 24.90, 15),
 ("Bowl for your guests car Keys", "Parties & Entertainment", 25.00, 2);
 
--- Updates the row where the column name is peter --
--- UPDATE products
--- SET department_name = true, price = "Franklin", stock_quantity = 2
--- WHERE id = 4;
+
 
 SELECT * FROM products;
